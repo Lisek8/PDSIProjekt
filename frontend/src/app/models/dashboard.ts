24dde4fce99a40647febb89c7c475a717c43733d
Lisek8 <fafik989@gmail.com>
2020-07-01 00:13:10 +0200

    Bugfixes

--- frontend/src/app/models/dashboard.ts
+++
@@ -1,7 +0,0 @@
-export interface Dashboard {
-    availableTopics: number;
-    inProgressTopics: number;
-    actionRequiredTopics: number;
-    finishedTopics: number;
-    newMessages: number;
-}
